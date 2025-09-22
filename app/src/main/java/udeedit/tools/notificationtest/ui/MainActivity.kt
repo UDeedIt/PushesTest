106a27a85061494b0be6da42f10515b4387e9739
Sargius <mailbox@sargius.pro>
2025-09-22 20:38:13 +0400

    - add send push notification feature - doesn't work "as is" since need manually set permission

--- app/src/main/java/udeedit/tools/notificationtest/ui/MainActivity.kt
+++ app/src/main/java/udeedit/tools/notificationtest/ui/MainActivity.kt
@@ -1,21 +1,86 @@
 package udeedit.tools.notificationtest.ui
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import udeedit.tools.notificationtest.R
+import udeedit.tools.notificationtest.databinding.ActivityMainBinding
+import udeedit.tools.notificationtest.utils.CHANNEL_ID
+import udeedit.tools.notificationtest.utils.CHANNEL_NAME
+import udeedit.tools.notificationtest.utils.NOTIF_ID
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        createNotificationsChannel()
+
+        binding.btnSendNotification.setOnClickListener {
+            publishNotification()
+        }
+    }
+
+    private fun createNotificationsChannel() {
+        Log.d(TAG, "createNotificationsChannel")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT).apply {
+                lightColor = Color.BLUE
+                enableLights(true)
+            }
+            val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            manager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun publishNotification() {
+        val intent=Intent(this, MainActivity::class.java)
+
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        val notif = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setContentTitle("Sample Title")
+            .setContentText("This is sample body notif")
+            .setSmallIcon(R.drawable.ic_alarm_bell)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        val notifManger = NotificationManagerCompat.from(this)
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            notifManger.notify(NOTIF_ID, notif)
         }
     }
 }
---
+++ app/src/main/java/udeedit/tools/notificationtest/utils/NTT_Constants.kt
@@ -0,0 +1,5 @@
+package udeedit.tools.notificationtest.utils
+
+val CHANNEL_ID = "channelID"
+val CHANNEL_NAME = "channelName"
+val NOTIF_ID = 0
